12cef2083e9e637b02780e2d4d1b6b72cfdaea9d
danielospina@gmail.com <danielospina@gmail.com>
2019-10-07 19:27:36 -0500

    contador de caracteres3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from'./Counter.js';
+//import Counter from'./Counter.js';
 
 class App extends Component {
 
-  
 
+  constructor(props) {
+   super(props);
+   this.state = {
+    value:0
+    }
+  };
+  count(event){
+  this.setState({ value: event.target.value.length });
+}
   render() {
     return (
-      <div className="container">
-      <Counter />
 
+      <div className="container">
+        <textarea rows="3" onChange={this.count.bind(this)}> </textarea>
+        <div className="counter">{this.state.value}</div>
       </div>
+
+
     );
   }
+
 }
 
 export default App;
